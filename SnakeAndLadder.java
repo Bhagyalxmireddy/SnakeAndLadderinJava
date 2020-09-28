9abd45b5ac5823b4460134f3bea1f154d56a67c0
Bhagi <puppyreddy1997@gmail.com>
2020-09-28 23:33:23 +0530

    [Bhagyalaxmi]Added:Generating the diec value using random

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,10 +3,17 @@ class SnakeAndLadder{
    static final int STARTINGPOSITION = 0;
    static final int LADDER = 1;
    static final int SNAKE = 2;
+   
+     public int gamePlayed()
+     {
+          double DICE = Math.floor(Math.random()*10)%6;
+     }
+
+
     public static void main(String args[])
     {
       System.out.println("Welcome to SnakeAndLadder Game");
      }
-
+ 
 
 }
