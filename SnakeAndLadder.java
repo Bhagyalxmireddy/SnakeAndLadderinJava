bbf05c5d09c3408669da57f85b2bf2273231390a
Bhagi <puppyreddy1997@gmail.com>
2020-09-29 00:54:56 +0530

    [Bhagyalaxmi]Added:generating the randomvalue to check the Action

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,18 +1,49 @@
 import java.util.*;
 class SnakeAndLadder{
    static final int STARTINGPOSITION = 0;
+   static final int ENDINGPOSITION = 100;
    static final int LADDER = 1;
    static final int SNAKE = 2;
-   
+   static final int NOPLAY = 0;
+   static String Action;
+   int CurrentPosition = 0;
      public int gamePlayed()
      {
-          double DICE = Math.floor(Math.random()*10)%6;
+         if(CurrentPosition < ENDINGPOSITION)
+           {
+               int DICE = (int)Math.floor(Math.random()*10)%6;
+               int ChooseOption = (int)Math.floor(Math.random()*10)%3;
+                 switch(ChooseOption)
+                  {
+                     case LADDER:
+                     if(CurrentPosition + DICE <= ENDINGPOSITION)
+                     {
+                         Action = "Ledder";
+                         CurrentPosition = CurrentPosition + DICE;
+                     }
+                     break;
+                      case SNAKE:
+                     if(CurrentPosition - DICE >= STARTINGPOSITION)
+                     {
+                          Action = "snake";
+                          CurrentPosition = CurrentPosition - DICE;
+                     }
+                     break;
+                     case NOPLAY:
+                      Action = "Noplay";
+                      CurrentPosition = CurrentPosition;
+                      break;
+                  }
+              System.out.println("Dice value is: " +DICE+ " for " +Action+ " and the currentpostion is: " +CurrentPosition);
+              }
+         return CurrentPosition;
      }
-
 
     public static void main(String args[])
     {
       System.out.println("Welcome to SnakeAndLadder Game");
+      SnakeAndLadder player = new SnakeAndLadder();
+      player.gamePlayed();
      }
  
 
