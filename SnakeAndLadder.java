904ee284c70e38df3b7ce36f2b270a62258ef897
Bhagi <puppyreddy1997@gmail.com>
2020-09-28 23:00:22 +0530

    [Bhagyalaxmi]Added:initializing the starting position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,8 @@
 import java.util.*;
 class SnakeAndLadder{
-
+   static final int STARTINGPOSITION = 0;
+   static final int LADDER = 1;
+   static final int SNAKE = 2;
     public static void main(String args[])
     {
       System.out.println("Welcome to SnakeAndLadder Game");
