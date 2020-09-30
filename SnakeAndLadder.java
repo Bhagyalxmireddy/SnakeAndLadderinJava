5bed560d27659c2126b087ff4bec74bde95f0026
Bhagi <puppyreddy1997@gmail.com>
2020-09-30 20:14:02 +0530

    [Bhagyalaxmi]Added:Printing dice value

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,22 +1,19 @@
 import java.util.*;
 class SnakeAndLadder{
-   static final int STARTINGPOSITION = 0;
-   static final int LADDER = 1;
-   static final int SNAKE = 2;
-   static Random random = new Random();
-     public static void gamePlayed()
-     {
-	//Random random = new random();
-        int dice = (random.nextInt(6)+1);
-	System.out.println("The Dicenumber is:" +dice);
-     }
-
-
-    public static void main(String args[])
-    {
-    	System.out.println("Welcome to SnakeAndLadder Game");
-        gamePlayed();
-    }
-
-
+	static final int STARTINGPOSITION = 0;
+   	static final int LADDER = 1;
+   	static final int SNAKE = 2;
+	static final int PLAYER = 0;
+   	static Random random = new Random();
+     		public static void gamePlayed()
+     		{
+			System.out.println("Game for single Player");
+        		int dice = (random.nextInt(6)+1);
+			System.out.println("The Dicenumber is:" +dice);
+     		}
+	public static void main(String args[])
+    	{
+    		System.out.println("Welcome to SnakeAndLadder Game");
+        	gamePlayed();
+    	}
 }
