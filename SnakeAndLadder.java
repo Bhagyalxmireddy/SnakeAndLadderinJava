8e90b4023a270955be183718f88038b977df548c
Bhagi <puppyreddy1997@gmail.com>
2020-10-01 17:07:23 +0530

    [Bhagyalaxmi]Refactered:Added random object

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,10 +4,10 @@ class SnakeAndLadder{
    	static final int LADDER = 1;
    	static final int SNAKE = 2;
 	static final int PLAYER = 0;
-   	static Random random = new Random();
      		public static void gamePlayed()
      		{
 			System.out.println("Game for single Player");
+			 Random random = new Random();
         		int dice = (random.nextInt(6)+1);
 			System.out.println("The Dicenumber is:" +dice);
      		}
